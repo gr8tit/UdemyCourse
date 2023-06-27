31c60647677195a1016395084b8b91e0d6ec48fd
temitopeoyedemi <temmygr8t@gmail.com>
2023-06-27 15:46:11 +0200

    first commit

--- src/main/java/PageObjects/CoreJavaBrushUp.java
+++ src/main/java/PageObjects/CoreJavaBrushUp.java
@@ -1,2 +1,81 @@
-package PageObjects;public class CoreJavaBrushUp {
+package PageObjects;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.apache.poi.sl.draw.geom.GuideIf.Op.val;
+
+public class CoreJavaBrushUp {
+    public static void main(String[] args){
+
+
+        //Array List
+//        int[] arr = new int[5];
+//
+//        arr[0] = 1;
+//        arr[1] = 2;
+//        arr[2] = 3;
+//        arr[3] = 4;
+//        arr[4] = 5;
+//
+//        int[] arr2 = {1,2,3,4,5,6};
+//        String[] arr3 = {"Temi","Tope","Mango"};
+
+//        for (int i = 0; i<arr.length; i++){
+//            System.out.println(arr[i]);
+//
+//        }
+//        for(int i = 0; i<arr2.length; i++){
+////            System.out.println(arr2[i]);
+//            if(arr2[i] % 2 == 0){
+//                System.out.println(arr2[i]);
+//                break;
+//            }else{
+//                System.out.println(arr2[1] + " not a multiple of 2");
+//            }
+//
+//        }
+
+//        //Array Collection
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<String> arrayList1 = new ArrayList<>();
+//        arrayList.add(4);
+//        arrayList.add(40);
+//        arrayList.add(141);
+//        arrayList.add(70);
+//
+//        arrayList1.add("lola");
+//        arrayList1.add("fifty");
+
+
+//
+//        for (int num : arr2) {
+//            arrayList.add(num);
+//        }
+//        arrayList.remove(arr2[2]);
+//
+//        System.out.println(arrayList.get(4));
+//        System.out.println(arrayList);
+
+//        for(int i = 0; i < arrayList.size(); i++){
+//            System.out.println(arrayList.get(i));
+//
+//        }
+//
+//        for(String val : arrayList1){
+//            System.out.println(val);
+//        }
+//
+//
+//        System.out.println(arrayList1.contains("fifty"));
+//        //Convert collection to array list
+//        String[] name = {"Oyedemi","Temitope","Loveth"};
+//        List<String> nameArrayList1 = Arrays.asList(name);
+//        nameArrayList1.contains("Oyedemi");
+
+
+        
+
+    }
 }
